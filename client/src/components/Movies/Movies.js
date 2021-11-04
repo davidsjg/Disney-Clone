74589b03550945c7ff1cc91aa1c294fcbec24715
Joey Davidson <j.davidson18@gmail.com>
2021-11-04 11:35:26 -0600

    Movies section complete, homepage design layout complete

--- client/src/components/Movies/Movies.js
+++ client/src/components/Movies/Movies.js
@@ -9,6 +9,15 @@ function Movies() {
         <Wrap>
           <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
         </Wrap>
+        <Wrap>
+          <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+        </Wrap>
+        <Wrap>
+          <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+        </Wrap>
+        <Wrap>
+          <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/6EA416AD3B15FCC1BADC817A932A57FFF707556DB2233FFCB4CFEB7C8EEDE23C/scale?width=400&aspectRatio=1.78&format=jpeg" />
+        </Wrap>
       </Content>
     </Container>
   );
@@ -18,6 +27,32 @@ export default Movies;
 
 const Container = styled.div``;
 
-const Content = styled.div``;
+const Content = styled.div`
+  display: grid;
+  grid-gap: 25px;
+  grid-template-columns: repeat(4, minmax(0, 1fr));
+  margin-bottom: 500px;
+`;
 
-const Wrap = styled.div``;
+const Wrap = styled.div`
+  overflow: hidden;
+  border-radius: 10px;
+  cursor: pointer;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: rgb(0 0 0 / 80%) 0px 26px 30px -10px,
+      rgb(0 0 0 / 72%) 0px 16px 10px -10px;
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
