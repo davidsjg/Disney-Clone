b08dcfe5e4b81310dc322590683440122206cb02
Joey Davidson <j.davidson18@gmail.com>
2021-11-04 19:50:28 -0600

    Detail page displaying movie info/subtitle, page complete

--- client/src/components/Detail/Detail.js
+++ client/src/components/Detail/Detail.js
@@ -26,6 +26,12 @@ function Detail() {
           <img src="/images/group-icon.png" />
         </GroupWatchButton>
       </Controls>
+      <SubTitle>2018 - 7m - Family, Fantasy, Kids, Animation</SubTitle>
+      <Description>
+        A Chinese mom who's sad when her grown son leaves home gets another
+        chance at motherhood when one of her dumplings springs to life. But she
+        finds that nothing stays cute and small forever.
+      </Description>
     </Container>
   );
 }
@@ -112,4 +118,18 @@ const AddButton = styled.button`
 `;
 const GroupWatchButton = styled(AddButton)`
   background: rgb(0, 0, 0);
+`;
+
+const SubTitle = styled.div`
+  font-size: 15px;
+  color: rgb(249, 249, 249);
+  min-height: 20px;
+  margin-top: 26px;
+`;
+
+const Description = styled.div`
+line-height: 1.4;
+font-size: 20px:
+margin-top: 16px;
+color: rgb(249, 249, 249);
 `;
